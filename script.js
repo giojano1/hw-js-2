cad6b5fd2ab9aca37c16ef7c4f1ce9c07edb212b
giojano1 <giorgi.janelidze64@gmail.com>
2024-04-14 17:47:54 +0400

    done

--- script.js
+++ script.js
@@ -3,23 +3,25 @@ let mainQuest = prompt(
 );
 let question;
 let answer;
+
 switch (mainQuest) {
   case "1":
-    question = prompt(`რა არის რა არის typeof(5)`);
+    question = `რა არის რა არის typeof(5)`;
     answer = "number";
     break;
   case "2":
-    question = prompt(`შეიძლება თუ არა let-ის რედეკლალირება`);
-    answer = "yes" || "YES" || "YeS";
+    question = `შეიძლება თუ არა let-ის რედეკლალირება`;
+    answer = "yes";
     break;
   case "3":
-    question = prompt(`რას აბრუნებს console.log(Math.floor(2.1))`);
-    answer = "ori" || "ORI" || "oRi";
+    question = `რას აბრუნებს console.log(Math.floor(2.1))`;
+    answer = "ori";
     break;
   default:
     alert("not found");
 }
-if (question === answer.toLowerCase()) {
+let answerUpdate = prompt(question);
+if (answer.toLowerCase() === answerUpdate.toLowerCase()) {
   alert("სწორი პასუხია");
 } else {
   alert("არასწორი პასუხია");
